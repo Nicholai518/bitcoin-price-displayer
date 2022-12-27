84329be79a1a91a1190dd4f98cf9fd637ce491c5
Nicholai <n.paz4444@gmail.com>
2022-12-27 15:00:02 -0500

    first commit

---
+++ src/main/java/com/nicholai518/bitcoinpricedisplayer/BitcoinPriceDisplayerApplication.java
@@ -0,0 +1,13 @@
+package com.nicholai518.bitcoinpricedisplayer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BitcoinPriceDisplayerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BitcoinPriceDisplayerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nicholai518/bitcoinpricedisplayer/BitcoinPriceDisplayerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nicholai518.bitcoinpricedisplayer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BitcoinPriceDisplayerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
